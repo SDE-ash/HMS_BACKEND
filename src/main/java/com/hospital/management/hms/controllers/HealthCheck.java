a20b00be158bca1c7fee713449eeec43b7d48487
SDE-ash <sdeveloperakash@gmail.com>
2025-07-18 11:20:39 +0530

    health-check

---
+++ src/main/java/com/hospital/management/hms/controllers/HealthCheck.java
@@ -0,0 +1,19 @@
+package com.hospital.management.hms.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hms-hc")
+public class HealthCheck {
+
+
+    @GetMapping
+    public ResponseEntity<?> getHealthCheck(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
